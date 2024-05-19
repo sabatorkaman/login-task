aeb3322bf1f0cd439bab7b8a64185f923f308a61
saba_torkaman <122853963+sabatorkaman@users.noreply.github.com>
2024-05-19 13:51:03 +0330

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        'em': '340px',
+        'sm': '450px',
+        // => @media (min-width: 640px) { ... }
+        'md': '880px',
+        'lg': '1024px',
+        // => @media (min-width: 1024px) { ... }
+  
+        'xl': '1280px',
+        // => @media (min-width: 1280px) { ... }
+      },
+    },
+  },
+  plugins: [],
+};
